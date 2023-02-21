05e5bf1ef41e6786c3e78b5edf8edfb8bf0575f4
Lasith Manujitha <Manujitha98@gmail.com>
2023-02-21 19:07:47 +0530

    Add Login route

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\UserAuthController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +15,13 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    if (!session() -> has('isLogged')) {
+        return redirect('login');
+    }
 });
+
+Route::get('/login', function() {
+    return view('login');
+});
+
+Route::post('/login/check', [UserAuthController::class, 'login']);
