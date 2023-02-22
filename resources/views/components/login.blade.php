a99892042d868318525d398d9d5a5b882fcda844
Lasith Manujitha <Manujitha98@gmail.com>
2023-02-23 00:12:51 +0530

    Update Login page layout

--- resources/views/components/login.blade.php
+++ resources/views/components/login.blade.php
@@ -12,23 +12,30 @@
 </head>
 
 <body>
-    <div class="container">
-        <form action="{{url('/login/check')}}" method="post">
-            @csrf
-            <div class="form-group">
-              <label for="exampleInputEmail1">Email</label>
-              <input type="email" class="form-control" name="email" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
-            </div>
-            <div class="form-group">
-              <label for="exampleInputPassword1">Password</label>
-              <input type="password" class="form-control" name="password" id="exampleInputPassword1" placeholder="Password">
-            </div>
-            <div class="form-check">
-              <input type="checkbox" class="form-check-input" id="exampleCheck1">
-              <label class="form-check-label" for="exampleCheck1">Remember me</label>
-            </div>
-            <button type="submit" class="btn btn-primary">Login</button>
-          </form>
+    <div class="container d-flex flex-column">
+
+      @if (session() -> has('loginError'))
+      <div class="alert alert-danger" role="alert">
+        {{session() -> get('loginError')}}
+      </div>
+      @endif
+
+      <form action="{{url('/login/check')}}" method="post">
+          @csrf
+          <div class="form-group">
+            <label>Email</label>
+            <input type="email" class="form-control mt-2" name="email" placeholder="Enter email">
+          </div>
+          <div class="form-group mt-3">
+            <label>Password</label>
+            <input type="password" class="form-control mt-2" name="password" placeholder="Password">
+          </div>
+          <div class="form-check mt-4 mb-4">
+            <input type="checkbox" class="form-check-input">
+            <label class="form-check-label">Remember me</label>
+          </div>
+          <button type="submit" class="btn btn-primary w-100">Login</button>
+        </form>
     </div>
 </body>
 
