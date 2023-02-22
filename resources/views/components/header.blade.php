e1c3c62e9b8dfcf74fa5df5feefb39b02755ff25
Lasith Manujitha <Manujitha98@gmail.com>
2023-02-23 00:39:51 +0530

    Admin Header component

---
+++ resources/views/components/header.blade.php
@@ -0,0 +1,32 @@
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"
+	integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+
+<body>
+	<nav class="navbar navbar-expand-lg bg-primary" data-bs-theme="dark">
+		<div class="container-fluid">
+			<a class="navbar-brand" href="#">Admin Dashboard</a>
+			<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+				aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+				<span class="navbar-toggler-icon"></span>
+			</button>
+			<div class="collapse navbar-collapse d-flex flex-row justify-content-between" id="navbarNav">
+				<ul class="navbar-nav">
+					<li class="nav-item">
+						<a class="nav-link active" aria-current="page" href="#">Dashboard</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="#">Customers</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="#">Employees</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="#">Products</a>
+					</li>
+				</ul>
+                <div>
+                    <a href="{{url('/logout')}}" class="btn btn-danger">Logout</a>
+                </div>
+			</div>
+		</div>
+	</nav>
