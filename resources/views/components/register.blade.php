cbe412e88d3dfdde2dbcf7b9a4b6f73a26b481ee
Lasith Manujitha <Manujitha98@gmail.com>
2023-02-23 14:09:38 +0530

    Update Registration page

--- resources/views/components/register.blade.php
+++ resources/views/components/register.blade.php
@@ -25,19 +25,19 @@
 			@csrf
 			<div class="form-group">
 				<label>Name</label>
-				<input type="text" class="form-control mt-2 mb-3" name="name" placeholder="Enter name" value="{{old('name')}}">
+				<input type="text" class="form-control mt-2 mb-3" name="name" placeholder="Enter name" value="{{old('name')}}" required>
 			</div>
 			<div class="form-group">
 				<label>Email</label>
-				<input type="email" class="form-control mt-2 mb-3" name="email" placeholder="Enter email" value="{{old('email')}}"">
+				<input type="email" class="form-control mt-2 mb-3" name="email" placeholder="Enter email" value="{{old('email')}}" required>
 			</div>
 			<div class="form-group">
 				<label>Address</label>
-				<input type="text" class="form-control mt-2 mb-3" name="address" placeholder="Enter address" value="{{old('address')}}"">
+				<input type="text" class="form-control mt-2 mb-3" name="address" placeholder="Enter address" value="{{old('address')}}" required>
 			</div>
 			<div class="form-group mb-3">
 				<lable>Gender</lable>
-				<select class="form-select" name="gender">
+				<select class="form-select" name="gender" required>
 					<option selected>Select a gender</option>
 					<option value="male">Male</option>
 					<option vlaue="female">Female</option>
@@ -45,16 +45,17 @@
 			</div>
 			<div class="form-group">
 				<label>Mobile</label>
-				<input type="number" class="form-control mt-2 mb-3" name="mobile" placeholder="Enter mobile" value="{{old('mobile')}}"">
+				<input type="number" class="form-control mt-2 mb-3" name="mobile" placeholder="Enter mobile" value="{{old('mobile')}}" required>
 			</div>
 			<div class="form-group mt-3">
 				<label>Password</label>
-				<input type="password" class="form-control mt-2 mb-3" name="password" placeholder="Password">
+				<input type="password" class="form-control mt-2 mb-3" name="password" placeholder="Password" required>
 			</div>
 			<div class="form-group">
 				<input type="hidden" class="form-control mt-2 mb-3" name="role" value="customer">
 			</div>
 			<button type="submit" class="btn btn-primary w-100">Register</button>
+			<a href="/login" style="text-decoration: none; color: white"><button type="button" class="btn btn-secondary w-100 mt-2">Login</button></a>
 		</form>
 	</div>
 </body>
