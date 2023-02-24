e425a60c66013dbeaec58a85a8c772cd7af873c4
Lasith Manujitha <Manujitha98@gmail.com>
2023-02-24 19:22:02 +0530

    Add products logic

--- resources/views/admin/addproducts.blade.php
+++ resources/views/admin/addproducts.blade.php
@@ -2,6 +2,22 @@
 @include('components/adminheader')
 
 <body>
-    <div class="d-flex align-items-center justify-content-center h-75">
-        <h1>this is add products page</h1>
+    <div class="container align-items-center justify-content-center h-75 w-50 mt-5">
+        <p class="text-center h3 mb-4">Add new product</p>
+        <form action="{{ url('admin/addproducts/check') }}" method="post">
+			@csrf
+			<div class="form-group">
+				<label>Name</label>
+				<input type="text" class="form-control mt-2 mb-3" name="name" placeholder="Enter name" value="{{old('name')}}" required>
+			</div>
+			<div class="form-group">
+				<label>Details</label>
+				<input type="text" class="form-control mt-2 mb-3" name="detail" placeholder="Enter address" value="{{old('detail')}}" required>
+			</div>
+            <div class="form-group">
+                <label>Price</label>
+                <input type="number" class="form-control mt-2 mb-3" name="price" placeholder="Enter price" value="{{old('price')}}" required>
+            </div>
+			<button type="submit" class="btn btn-primary w-100">Add Product</button>
+		</form>
     </div>
--- resources/views/admin/products.blade.php
+++ resources/views/admin/products.blade.php
@@ -2,15 +2,16 @@
 @include('components/adminheader')
 
 <body>
-	<div class="d-flex align-items-center justify-content-center">
-		<table class="table w-75 table-striped table-bordered mt-4">
+	<div class="d-flex flex-column align-items-center justify-content-center">
+      <a href="addproducts" class="btn btn-primary mt-3">Add new Product</a>
+		<table class="table table-striped table-bordered mt-4" style="width: 90%">
          <thead class="thead-dark">
 				<tr>
 					<th scope="col">ID</th>
 					<th scope="col">Name</th>
 					<th scope="col">Details</th>
 					<th scope="col">Price</th>
-               <th scope="col">Action</th>
+               <th scope="col" style="width: 220px">Action</th>
 				</tr>
 			</thead>
 			<tbody>
@@ -22,7 +23,7 @@
 						<td>{{ $row['price'] }}</td>
                   <td>
                      <a href="{{ route('admin.showproduct', $row['id'])}}" class="btn btn-success">Show</a>
-                     <a href="{{ route('admin.addproduct', $row['id']), ['action' => 'edit'] }}" class="btn btn-primary">Edit</a>
+                     <a href="{{ route('admin.editproduct', $row['id'])}}" class="btn btn-primary">Edit</a>
                      <a href="{{ route('admin.deleteproduct', $row['id'])}}" class="btn btn-danger">Delete</a>
                   </td>
 					</tr>
