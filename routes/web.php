fc0b36b08a9a5ade6b8bedb34aa97f32c4ddc371
Lasith Manujitha <Manujitha98@gmail.com>
2023-02-24 19:03:15 +0530

    Update routes for Products

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProductsController;
 use App\Http\Controllers\UserAuthController;
 use Illuminate\Support\Facades\Route;
 
@@ -14,92 +15,99 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function() {
-    return redirect() -> route('login');
+Route::get('/', function () {
+    return redirect()->route('login');
 });
 
-Route::get('/login', function() {
-    if (session() -> has('isLogged') && session() -> get('isLogged') == true) {
-        switch (session() -> get('role')) {
+Route::get('/login', function () {
+    if (session()->has('isLogged') && session()->get('isLogged') == true) {
+        switch (session()->get('role')) {
             case 'admin':
-                return redirect() -> route('admin.dashboard');
+                return redirect()->route('admin.dashboard');
                 break;
             case 'customer':
-                return redirect() -> route('customer.dashboard');
+                return redirect()->route('customer.dashboard');
                 break;
             case 'employee':
-                return redirect() -> route('employee.dashboard');
+                return redirect()->route('employee.dashboard');
             default:
-                return redirect() -> route('login');
+                return redirect()->route('login');
                 break;
         }
     }
 
     return view('components.login');
-}) -> name('login');
+})->name('login');
 
-Route::get('/register', function() {
+Route::get('/register', function () {
     return view('components.register');
-}) -> name('register');
+})->name('register');
 
 Route::post('/login/check', [UserAuthController::class, 'login']);
 Route::post('/register/check', [UserAuthController::class, 'register']);
 
-Route::group(['prefix' => 'admin', 'middleware' => ['admin']], function() {
-    Route::get('/', function() {
-        return redirect() -> route('admin.dashboard');
+Route::group(['prefix' => 'admin', 'middleware' => ['admin']], function () {
+    Route::get('/', function () {
+        return redirect()->route('admin.dashboard');
     });
 
-    Route::get('/dashboard', function() {
+    Route::get('/dashboard', function () {
         return view('admin.dashboard');
-    }) -> name('admin.dashboard');
+    })->name('admin.dashboard');
 
-    Route::get('/products', function() {
-        return view('admin.products');
-    }) -> name('admin.products');
+    // Products routes
+    Route::get('/products', [ProductsController::class, 'showProducts'])->name('admin.products');
 
-    Route::get('/addproducts', function() {
+    Route::get('/addproducts', function () {
         return view('admin.addproducts');
-    }) -> name('admin.addproducts');
+    })->name('admin.addproducts');
 
     Route::post('/addproducts/check', [ProductsController::class, 'addProduct']);
-    
 
-    Route::get('/employees', function() {
+    // Actions for product
+    Route::get('/showproduct', function () {
+        return view('admin.showproduct');
+    })->name('admin.showproduct');
+
+    Route::get('/editproduct/{id}', [ProductsController::class, 'editProduct'])->name('admin.editproduct');
+
+    Route::get('/deleteproduct/{id}', [ProductsController::class, 'deleteProduct'])->name('admin.deleteproduct');
+
+    // Employee routes
+    Route::get('/employees', function () {
         return view('admin.employees');
     });
-
 });
 
-Route::group(['prefix' => 'customer', 'middleware' => ['customer']], function() {
-    Route::get('/', function() {
-        return redirect() -> route('customer.dashboard');
+Route::group(['prefix' => 'customer', 'middleware' => ['customer']], function () {
+    Route::get('/', function () {
+        return redirect()->route('customer.dashboard');
     });
 
-    Route::get('/dashboard', function() {
+    Route::get('/dashboard', function () {
         return view('customer.dashboard');
-    }) -> name('customer.dashboard');
+    })->name('customer.dashboard');
 
-    Route::get('/order', function() {
+    Route::get('/order', function () {
         return view('customer.order');
     });
 });
 
-Route::group(['prefix' => 'employee', 'middleware' => ['employee']], function() {
-    Route::get('/', function() {
-        return redirect() -> route('employee.dashboard');
+Route::group(['prefix' => 'employee', 'middleware' => ['employee']], function () {
+    Route::get('/', function () {
+        return redirect()->route('employee.dashboard');
     });
 
-    Route::get('/dashboard', function() {
+    Route::get('/dashboard', function () {
         return view('customer.dashboard');
-    }) -> name('customer.dashboard');
+    })->name('customer.dashboard');
 
-    Route::get('/order', function() {
+    Route::get('/order', function () {
         return view('customer.order');
     });
 });
 
-Route::get('/logout', function() {
-    session() -> flush();
-    return redirect() -> route('login');
+Route::get('/logout', function () {
+    session()->flush();
+    return redirect()->route('login');
 });
