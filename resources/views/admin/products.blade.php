cd2a441aa04efa82ec0978942ff87ce1e6694720
Lasith Manujitha <Manujitha98@gmail.com>
2023-02-24 13:50:34 +0530

    Add show products table

--- resources/views/admin/products.blade.php
+++ resources/views/admin/products.blade.php
@@ -1,9 +1,33 @@
-{{session() -> put('current', 'products')}}
+{{ session()->put('current', 'products') }}
 @include('components/adminheader')
 
 <body>
-    <div class="d-flex align-items-center justify-content-center h-75">
-        <h1>this is products page</h1>
-    </div>
-    
-@include('components/footer')
+	<div class="d-flex align-items-center justify-content-center">
+		<table class="table w-75 table-striped table-bordered mt-4">
+         <thead class="thead-dark">
+				<tr>
+					<th scope="col">ID</th>
+					<th scope="col">Name</th>
+					<th scope="col">Details</th>
+					<th scope="col">Price</th>
+               <th scope="col">Action</th>
+				</tr>
+			</thead>
+			<tbody>
+				@foreach ($products as $row)
+					<tr>
+						<td><strong>{{ $row['id'] }}</strong></td>
+						<td>{{ $row['name'] }}</td>
+						<td>{{ $row['detail'] }}</td>
+						<td>{{ $row['price'] }}</td>
+                  <td>
+                     <a href="{{ route('admin.showproduct', $row['id'])}}" class="btn btn-success">Show</a>
+                     <a href="{{ route('admin.addproduct', $row['id']), ['action' => 'edit'] }}" class="btn btn-primary">Edit</a>
+                     <a href="{{ route('admin.deleteproduct', $row['id'])}}" class="btn btn-danger">Delete</a>
+                  </td>
+					</tr>
+				@endforeach
+			</tbody>
+		</table>
+
+	</div>
